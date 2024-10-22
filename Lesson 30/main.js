825255606612a99fdd70faebf9fc0096503b91dd
andirrahmani <andirrahmani28@gmail.com>
2024-10-22 16:21:32 +0200

    lesson 30

---
+++ Lesson 30/main.js
@@ -0,0 +1,21 @@
+var city = prishtina
+var age = 18
+
+
+if(city= "Prishtina" && age>="18") 
+    {
+    console.log("You are a citizen of prishtina")
+}
+else{
+    console,log("You are not a citizen of prishtina")
+}
+
+
+var cars= ["Bmw", "Audi", "Mercedes", "Volkswagen", "Nissan"]
+
+if(cars[5]=="Nissan"){
+    console.log( "It is")
+}
+else{
+    console.log("It is not")
+}
