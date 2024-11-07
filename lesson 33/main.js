9b927d1af4a7bdb7d2d82159e7d1a6e05d60f4cc
andirrahmani <andirrahmani28@gmail.com>
2024-11-07 16:20:46 +0100

    lesson 33

---
+++ lesson 33/main.js
@@ -0,0 +1,25 @@
+function validate(){
+    var name = document.getElementById("name").value;
+    var name_regex = "/^[A-Za-z]+$/";
+
+    var age = document.getElementById("age").value;
+    var name_regex = "^[0-9]+$/";
+
+    var city = document.getElementById("city").value;
+
+    if(!(name.match(name_regex)) || !(age.match(age_regex)) || (city=="")){
+        if(!(name.match(name_regex))){
+            document.getElementById("name_error").style.visibility = "visible";     
+        }
+        if(!(age.match(age_regex))){
+            document.getElementById("age_error").style.visibility = "visible";     
+        }
+        if(city==""){
+            document.getElementById("city_error").style.visibility = "visible";     
+        }
+        return false;
+    }
+    else{
+        return true;
+    }
+}
