03d327d34f84c71b16c7d92f183b2166e8f80825
AndiRrahmani <andirrahmani28@gmail.com>
2024-11-21 16:17:20 +0100

    lesson35

---
+++ Hamburger menu/main.js
@@ -0,0 +1,10 @@
+function openMenu(){
+    var menu = document.getElementById('links')
+
+    if(menu.style.display == "none"){
+        menu.style.display = "block"
+    }
+    else{
+        menu.style.display = "none"
+    }
+}
